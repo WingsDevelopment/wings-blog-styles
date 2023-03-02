5cbac1f1d66bed2671beb5373389c41c20f81b13
Srdjan Rakic <srdjanr.1609@gmail.com>
2023-03-02 22:30:58 +0100

    fixed build error

--- wings-blog-styles/src/lib/components/Title.svelte
+++ wings-blog-styles/src/lib/components/Title.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
+	import Img from '$lib/images/titleshape.svg';
 	export let title: string;
 </script>
 
 <div class="titleCol text-center">
-	<img class="mx-auto" src="/src/lib/images/titleshape.svg" alt="Title Shape" />
+	<img class="mx-auto" src={Img} alt="Title Shape" />
 	<h3 class="text-4xl text-dark font-semibold">{title}</h3>
 </div>
