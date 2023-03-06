a8a93ad7d9835c6362c6000c21395ec2bcab6af9
vikrambachhal <vs8607438070@gmail.com>
2023-03-06 20:12:34 +0530

    Navigation add

--- wings-blog-styles/src/lib/components/Nav.svelte
+++ wings-blog-styles/src/lib/components/Nav.svelte
@@ -1,58 +1,60 @@
 <script>
 	import Tag from '$lib/components/Tag.svelte';
+	import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+import '@splidejs/splide/dist/css/themes/splide-default.min.css';
 </script>
 
-<div class="navCol w-full scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-primary scrollbar-track-primaryLight px-2 overflow-x-auto">
-	<ul class="whitespace-nowrap">
-		<li class="active">
+<div class="navCol w-full  px-2 overflow-x-auto">
+	<Splide options={{ autoWidth: true,omitEnd: true,focus: 0,perMove: 1,}} aria-label="My Favorite Images">
+		<SplideSlide>
 			<button>
 				<Tag tagText="All" />
 			</button>
-		</li>
-		<li>
-			<button>
+		</SplideSlide>
+		<SplideSlide>
+			<button> 
 				<Tag tagText="Programming" />
 			</button>
-		</li>
-		<li>
-			<button>
+		</SplideSlide>
+		<SplideSlide>
+			<button> 
 				<Tag tagText="Data Science" />
 			</button>
-		</li>
-		<li>
-			<button>
+		</SplideSlide>
+		<SplideSlide>
+			<button> 
 				<Tag tagText="Technology" />
 			</button>
-		</li>
-		<li>
-			<button>
+		</SplideSlide>
+		<SplideSlide>
+			<button> 
 				<Tag tagText="Improvement" />
 			</button>
-		</li>
-		<li>
-			<button>
+		</SplideSlide>
+		<SplideSlide>
+			<button> 
 				<Tag tagText="Writing" />
 			</button>
-		</li>
-		<li>
-			<button>
+		</SplideSlide>
+		<SplideSlide>
+			<button> 
 				<Tag tagText="Relationships" />
 			</button>
-		</li>
-		<li>
-			<button>
+		</SplideSlide>
+		<SplideSlide>
+			<button> 
 				<Tag tagText="Machine Learning" />
 			</button>
-		</li>
-		<li>
-			<button>
+		</SplideSlide>
+		<SplideSlide>
+			<button> 
 				<Tag tagText="Productivity" />
 			</button>
-		</li>
-		<li>
-			<button>
+		</SplideSlide>
+		<SplideSlide>
+			<button> 
 				<Tag tagText="Politics" />
 			</button>
-		</li>
-	</ul>
+		</SplideSlide>
+	</Splide>
 </div>
